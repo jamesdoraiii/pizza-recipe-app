c189d719b589b31ec455ba9a3a58068ddbe43c06
James Dora <jamesdoraiii@gmail.com>
2020-04-19 23:45:18 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { RecipesForCategoryComponent } from './views/recipes-for-category/recipes-for-category.component';
+import { RecipeDetailComponent } from './views/recipe-detail/recipe-detail.component';
+import { RecipeCardComponent } from './components/recipe-card/recipe-card.component';
+import { RecipeListItemComponent } from './components/recipe-list-item/recipe-list-item.component';
+import { NavbarComponent } from './components/shell/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecipesForCategoryComponent,
+    RecipeDetailComponent,
+    RecipeCardComponent,
+    RecipeListItemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/recipes-for-category/recipes-for-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-for-category',
+  templateUrl: './recipes-for-category.component.html',
+  styleUrls: ['./recipes-for-category.component.scss']
+})
+export class RecipesForCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
